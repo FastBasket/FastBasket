0420bee6da4f3776deb8685d7383ad5dd51ab6fd
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2016-01-21 16:34:55 -0800

    add error handlers

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,4 +4,13 @@ var storeController = require('../controllers/storeController');
 module.exports = function (app, express) {
   app.get('/api/product/getProducts', productController.getProducts);
   app.get('/api/store/getStores', storeController.getStores);
+
+  app.use(function (error, req, res, next) {
+    console.error(error.stack);
+    next(error);
+  });
+
+  app.use(function (error, req, res, next) {
+    res.status(500).send({ error: error.message });
+  });
 };
