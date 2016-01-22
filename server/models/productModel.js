bb2fe70b560842daebab8229f3b71e1943be7989
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2016-01-21 17:17:06 -0800

    add pg-promise

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -1,20 +1,15 @@
-var pg = require('pg');
-var constants = require('../config/constants');
+var db = require('../db/db');
 
 module.exports = {
 
   getProducts: function(callback){
-    pg.connect(constants.DATABASE_URL, function(err, client, done){
-      if(err) {
-        console.error('error fetching client from pool', err);
-        callback(err, null);
-      }
-
-      client.query('Select * from Products', function(err, products){
-        done();
-        callback(err, products.rows);
+    db.query('Select * from Products')
+      .then(function(products){
+        callback(null, products);
+      })
+      .catch(function(error){
+        callback(error, null);
       });
-    });
   }
 
 };
--- server/models/storeModel.js
+++ server/models/storeModel.js
@@ -1,20 +1,15 @@
-var pg = require('pg');
-var constants = require('../config/constants');
+var db = require('../db/db');
 
 module.exports = {
 
   getStores: function(callback){
-    pg.connect(constants.DATABASE_URL, function(err, client, done){
-      if(err) {
-        console.error('error fetching client from pool', err);
-        callback(err, null);
-      }
-
-      client.query('Select * from Stores', function(err, stores){
-        done();
-        callback(err, stores.rows);
+    db.query('Select * from Stores')
+      .then(function(stores){
+        callback(null, stores);
+      })
+      .catch(function(error){
+        callback(error, null);
       });
-    });
-  }
+  },
 
 };
