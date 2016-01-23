667aa37119614e1a02d451768bc9a46bddc4b499
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2016-01-23 15:52:59 -0800

    add search header

---
+++ client/js/controllers/searchBarController.js
@@ -0,0 +1,47 @@
+angular.module('fastBasket.searchBar', [])
+.controller('searchBarController', function($scope, $http, $rootScope, $state){
+  function elasticSearch(text){
+    return $http({
+      method: 'GET',
+      url: '/api/product/search/' + text
+    })
+    .then(function(products){
+      return products.data;
+    });
+  }
+
+  this.querySearch = function(text){
+    if (text && text.trim() !== ''){
+      return elasticSearch(text);
+    } else {
+      return [];
+    }
+  };
+  
+  this.searchTextChange = function(text){
+    if (text && text.trim() !== ''){
+      return elasticSearch(text);
+    } else {
+      return [];
+    }
+  };
+
+  this.selectedItemChange = function(item){
+    if (item && item.name.trim() !== ''){
+      $http({
+        method: 'GET',
+        url: '/api/product/showResults/' + item.name
+      })
+      .then(function(products){
+        $rootScope.products = products.data;
+      });
+    }
+  };
+
+  this.checkEnter = function($event){
+    var keycode = $event.which || $event.KeyCode;
+    if (keycode === 13){
+      this.selectedItemChange({name: this.searchText});
+    }
+  };
+});
