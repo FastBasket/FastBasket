d1b69c54e3b6eaded836620e90128137dc00ead8
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2016-01-26 09:35:27 -0800

    remove trace log

--- server/elastic.js
+++ server/elastic.js
@@ -2,8 +2,7 @@ var elasticsearch = require('elasticsearch');
 var constants = require('./config/constants');
 
 var client = new elasticsearch.Client({
-  host: constants.ELASTIC_URL,
-  log: 'trace'
+  host: constants.ELASTIC_URL
 });
 
 module.exports = client;
