0d735ecd200b7351ff09bab83343ffe580aa0d14
Ron Lapushner <lapushnerron@gmail.com>
2016-02-04 16:55:38 -0800

    removed grunt command variables

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,5 +29,5 @@ module.exports = function(grunt) {
   //grunt.loadNpmTasks('grunt-contrib-uglify');
   //grunt.loadNpmTasks('grunt-contrib-concat');
 
-  grunt.registerTask('start', ['env:prod', 'shell']);
+  grunt.registerTask('start', [ 'shell']);
 }
