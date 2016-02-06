021b937f82170b89788706a841c9430fb84c6fd9
Ron Lapushner <lapushnerron@gmail.com>
2016-02-06 11:00:41 -0800

    Improved automation with grunt and neo db scriptw

--- rec/recScript/index.js
+++ rec/recScript/index.js
@@ -1,66 +1,54 @@
+var Promise = require('bluebird');
 var neo4j = require('neo4j');
-var db = new neo4j.GraphDatabase('http://neo4j:fastbasket@127.0.0.1:7474');
-var fs = require('fs');
-
-var recipes = fs.readFileSync('test.json');
+var db = Promise.promisifyAll(new neo4j.GraphDatabase('http://neo4j:fastbasket@127.0.0.1:7474'));
+var fs = Promise.promisifyAll(require('fs'));
+var recipes = fs.readFileSync(__dirname + '/test.json');
 var recipes = JSON.parse(recipes);
 var totalRec = recipes.length;
 
-//Make sure "ulimit -n" returns ~40000;
-recipes.forEach(function (recipe, index) {
-  console.log(index / totalRec)
+  //Make sure "ulimit -n" returns ~60000 and use flag --max-old-space-size=8192;
+Promise.map(recipes, function(recipe){
   recipe.ingredients.forEach(function (ingredient) {
-    db.cypher({
-        query: 'MERGE (item:Food {name: {name}}) return item',
-        params: {
-            name: ingredient,
-        }
-    },function (err, results) {
-      if(err){console.log(err)};
+    return db.cypherAsync({
+      query: 'MERGE (item:Food {name: {name}}) return item',
+           params: {
+             name: ingredient,
+           }
+    }).then(function (results) {
       console.log(results)
+    }).catch(function(err){
+      if(err){console.log(err)};
     });
   })
-})
+}).then(function(){
+  console.log('all products have been inserted...');
 
-setTimeout(function () {
-  recipes.forEach(function (recipe, index) {
-    db.cypher({
-        query: 'MERGE (item:Order {index: {index}}) RETURN item.index',
-        params: {
-            index: index
-        }
-    },function(err, index){
-        db.cypher({
-            query: 'MATCH (a:Order {index: {index}}), (b:Food) WHERE b.name in {ingredients} MERGE (a)-[r:Contains]->(b)',
-            params: {
-                index: index[0]['item.index'],
-                ingredients: recipes[index[0]['item.index']].ingredients
-            }
-        }, function (err, result) {
-
-        })
-    });
+  return Promise.map(recipes, function(recipe, index){
+    return db.cypherAsync({
+      query: 'MERGE (item:Order {index: {index}})',
+           params: {
+             index: index
+           }
+    })
   })
-}, 15000);
+} , { concurrency: 3 }).then(function(){
+  console.log('all Orders have been inserted...');
 
-
-// match (a)-[r:Contains]->(b) where a.index in [5,11,9,8] and not b.name in [
-//       "baking powder",
-//       "eggs",
-//       "all-purpose flour",
-//       "raisins",
-//       "milk",
-//       "white sugar"
-//     ] return b, count(r) as count order by count desc
-
-// match (a)-[r:Contains]->(b) where b.name in [
-//       "baking powder",
-//       "eggs",
-//       "all-purpose flour",
-//       "raisins",
-//       "milk",
-//       "white sugar"
-//     ] return a, count(r) as count order by count desc limit 4
-
+  recipes.forEach(function(recipe, index){
+    (function(index){
+      db.cypherAsync({
+        query: 'MATCH (a:Order {index: {index}}), (b:Food) WHERE b.name in {ingredients} MERGE (a)-[r:Contains]->(b)',
+      params: {
+        index: index,
+      ingredients: recipes[index].ingredients
+      }
+      }).catch(function (err) {
+        console.log(err);
+      })
+    })(index);
+  }, { concurrency: 3 })
+}).catch(function (err) {
+  console.log(err);
+});
 
 module.exports = db;
