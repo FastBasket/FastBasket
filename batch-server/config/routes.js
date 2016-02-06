21e8f7b30d08c4709bdc8912106bcb1c3c083fb8
Albert Tsou <tsou.albert@gmail.com>
2016-02-05 19:07:50 -0800

    aggregator with rabbit mq

--- batch-server/config/routes.js
+++ batch-server/config/routes.js
@@ -1,46 +1,83 @@
 var amqp = require('amqplib/callback_api');
 var kmeans = require('./kmeans.js');
 
-module.exports = function (app, redis) {
-  redis.on("message", function (channel, message) {
-    console.log("client1 channel " + channel + ": " + message);
+module.exports = function (redis) {
 
-    message = JSON.parse(message);
-    if (message.len > 2) {
-      redis.unsubscribe();
-      redis.lrange(message.storeId, 0, 9, function(err, reply){
-        redis.ltrim('1', 10, -1);
-        console.log(reply);
+  amqp.connect('amqp://localhost', function(err, conn) {
+    conn.createChannel(function(err, ch) {
+      var q_out = 'q_out';
+      var q = 'task_queue';
 
-        var data = reply.map(function(ele){
-          return JSON.parse(ele);
-        });
+      ch.assertQueue(q_out, {durable: true});
+      console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q_out);
+      ch.assertQueue(q, {durable: true});
+      console.log(" [*] Established connection with %s. To exit press CTRL+C", q);
 
-        // process data
-          // grab from postgres
 
-          var buckets = kmeans(data ,4 , 10);
+      ch.prefetch(1);
+      ch.consume(q_out, function(msg) {
 
-          // queue up
-          amqp.connect('amqp://localhost', function(err, conn) {
-            conn.createChannel(function(err, ch) {
-              var q = 'task_queue';
-                for(var key in buckets){
+        console.log(" [x] Received %s", msg.content.toString());
 
-                  if(buckets[key].length > 0){
-                    var msg = JSON.stringify({data: buckets[key]});
+        var data = JSON.parse(msg.content.toString())
+        var buckets = kmeans(data ,4 , 10);
+        for(var key in buckets){
 
-                    ch.assertQueue(q, {durable: true});
-                    ch.sendToQueue(q, new Buffer(msg), {persistent: true});
-                    console.log(" [x] Sent '%s'", msg);
-                  }
-                }
-            });
-            setTimeout(function() { conn.close(); }, 500);
-          });
+          if(buckets[key].length > 0){
+            var job = JSON.stringify({data: buckets[key]});
 
-          redis.subscribe("jobs");
-      });
-    }
+            ch.assertQueue(q, {durable: true});
+            ch.sendToQueue(q, new Buffer(job), {persistent: true});
+            console.log(" [x] Sent '%s'", job);
+          }
+        }
+        console.log(" [x] Done");
+        ch.ack(msg);
+      }, {noAck: false});
+    });
   });
+
+
+
+  // redis.on("message", function (channel, message) {
+  //   console.log("client1 channel " + channel + ": " + message);
+  //
+  //   message = JSON.parse(message);
+  //   if (message.len > 2) {
+  //     redis.unsubscribe();
+  //     redis.lrange(message.storeId, 0, 9, function(err, reply){
+  //       redis.ltrim('1', 10, -1);
+  //       console.log(reply);
+  //
+  //       var data = reply.map(function(ele){
+  //         return JSON.parse(ele);
+  //       });
+  //
+  //       // process data
+  //         // grab from postgres
+  //
+  //         var buckets = kmeans(data ,4 , 10);
+  //
+  //         // queue up
+  //         amqp.connect('amqp://localhost', function(err, conn) {
+  //           conn.createChannel(function(err, ch) {
+  //             var q = 'task_queue';
+  //               for(var key in buckets){
+  //
+  //                 if(buckets[key].length > 0){
+  //                   var msg = JSON.stringify({data: buckets[key]});
+  //
+  //                   ch.assertQueue(q, {durable: true});
+  //                   ch.sendToQueue(q, new Buffer(msg), {persistent: true});
+  //                   console.log(" [x] Sent '%s'", msg);
+  //                 }
+  //               }
+  //           });
+  //           setTimeout(function() { conn.close(); }, 500);
+  //         });
+  //
+  //         redis.subscribe("jobs");
+  //     });
+  //   }
+  // });
 };
--- server/controllers/checkoutController.js
+++ server/controllers/checkoutController.js
@@ -5,6 +5,7 @@ var constants = require('../config/constants');
 var stripe = require("stripe")(
   process.env.STRIPE_SECRET_KEY
 );
+var amqp = require('amqplib/callback_api');
 
 module.exports = {
 
@@ -14,7 +15,7 @@ module.exports = {
       res.status(200).json(status);
     });
   },
-  
+
   charge: function(req, res, next){
     var stripeToken = req.body.stripeToken;
     var amount = req.body.amount;
@@ -42,14 +43,31 @@ module.exports = {
         console.log(err);
         res.sendStatus(400);
       } else {
-        redis.rpush(request.storeId, JSON.stringify({ x: request.x, y: request.y, id: order.id }), function(err, redisRes){
-          if (err){
-            console.log('error from redis', err);
-          } else {
-            redis.publish('jobs', JSON.stringify({ len: redisRes, storeId: request.storeId }));
-          }
-          res.status(200).json(order);
+        console.log('made it here')
+
+        amqp.connect('amqp://localhost', function(err, conn) {
+          conn.createChannel(function(err, ch) {
+            var q = 'q_in';
+            console.log('connected to q_in')
+
+
+            var msg = JSON.stringify({ x: request.x, y: request.y, id: order.id, storeId: request.storeId });
+            console.log(msg)
+            ch.assertQueue(q, {durable: true});
+            ch.sendToQueue(q, new Buffer(msg), {persistent: true});
+            console.log(" [x] Sent '%s'", msg);
+            res.status(200).json(order);
+          });
         });
+
+        // redis.rpush(request.storeId, JSON.stringify({ x: request.x, y: request.y, id: order.id }), function(err, redisRes){
+        //   if (err){
+        //     console.log('error from redis', err);
+        //   } else {
+        //     redis.publish('jobs', JSON.stringify({ len: redisRes, storeId: request.storeId }));
+        //   }
+        //   res.status(200).json(order);
+        // });
       }
     });
   }
