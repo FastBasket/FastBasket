d7ae583b7975d9a91a1360dfef1daf39dd32bb8d
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2016-02-11 19:28:38 -0800

    redirect splash pages

--- driver-client/js/app.js
+++ driver-client/js/app.js
@@ -1,20 +1,22 @@
 var driverSide = angular.module("driverSide",
     ['ui.router',
     'ngMaterial',
-    'driverSide.profile']);
+    'driverSide.profile',
+    'fastBasket.login']);
 
 driverSide.config(function($stateProvider, $urlRouterProvider, $mdThemingProvider) {
     $stateProvider
     .state('login', {
         url: "/login",
-        templateUrl:'templates/login.html'
+        templateUrl:'templates/login.html',
+        controller: 'loginController'
     })
     .state('profile', {
         url: '/profile',
         templateUrl: 'templates/profile.html',
         controller: 'profileController'
     });
-    
+
     $urlRouterProvider
     .otherwise('/login');
 
